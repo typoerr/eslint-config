f37b59be6b8be7e7e1f283e6cd32aa8a8e1376bb
nullabletypo <masahirokato.dev@gmail.com>
2019-02-23 08:14:17 +0900

    split configs

---
+++ +typescript.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-this-alias': 'error',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+  },
+}
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./.eslintrc.json')
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'no-sequences': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'default-case': 'error',
+    eqeqeq: ['error', 'smart'],
+    'no-var': 'error',
+    yoda: 'error',
+    'one-var': ['error', { initialized: 'never' }],
+  },
+}
