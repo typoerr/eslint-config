e110b3270b241366dc95cd34ce6c1dd1aab25555
typoerr <masahirokato.dev@gmail.com>
2020-05-24 15:36:16 +0900

    chore: update typescript rule

--- +typescript.js
+++ +typescript.js
@@ -32,6 +32,7 @@ module.exports = {
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-inferrable-types': 'off',
