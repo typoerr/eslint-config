9163058c97ee972d0d02127a346c3f0d4d0449ac
typoerr <masahirokato.dev@gmail.com>
2021-09-20 08:46:11 +0900

    feat: +react.js

---
+++ +react.js
@@ -0,0 +1,5 @@
+// [yannickcr/eslint-plugin-react: React specific linting rules for ESLint]
+// (https://github.com/yannickcr/eslint-plugin-react "yannickcr/eslint-plugin-react: React specific linting rules for ESLint")
+module.exports = {
+  extends: ['plugin:react/recommended'],
+}
